653f523cc65339b14773ef841ca8b7a62a50bff4
PCBhagwat <P.C.Bhagwat@tudelft.nl>
2012-12-12 17:43:17 +0100

    Biased coin created for regulator

--- src/main/java/polep/role/RegulatorRole.java
+++ src/main/java/polep/role/RegulatorRole.java
@@ -3,6 +3,15 @@ package polep.role;
 import agentspring.role.AbstractRole;
 import polep.domain.agent.Regulator;
 
+/*<--- RegulatorRole: Prad ----->
+Regulator controls Powerplant owner:
+	Throw a weighted coin (probability to control) to select owners for control (this is to simulate imperfect knowledge of the regulator)
+	Compare Bid volume with installed capacity
+	If Bid volume is less than installed capacity = Fine
+	Fine is property of regulator
+	Update Cash = Cash - Fine */
+
+
 public abstract class RegulatorRole extends AbstractRole<Regulator> {
 
 }
