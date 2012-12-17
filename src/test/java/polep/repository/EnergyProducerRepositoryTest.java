035f91ee460718070389845ea1ccaf4f18773f09
JCRichstein <j.c.richstein@tudelft.nl>
2012-12-17 16:25:57 +0100

    Finished up EnergyProducerRepositoryTest.

--- src/test/java/polep/repository/EnergyProducerRepositoryTest.java
+++ src/test/java/polep/repository/EnergyProducerRepositoryTest.java
@@ -26,13 +26,22 @@ public class EnergyProducerRepositoryTest {
     @Autowired EnergyProducerRepository energyProducerRepository;
 
 	@Test
-	public void testFindAll() {
+	public void testRepositoryStandardFunctions() {
 		EnergyProducer energyProducer1 = new EnergyProducer();
 		energyProducer1.setName("Producer1");
 		energyProducer1.persist();
 		
 		EnergyProducer energyProducerFromRepository = energyProducerRepository.findAll().iterator().next();
 		assertTrue("Check if producer name is the same", energyProducerFromRepository.getName().equals(energyProducer1.getName()));
+		
+		for(int i = 2; i<=10; i++){
+			EnergyProducer energyProducer = new EnergyProducer();
+			energyProducer.setName("Producer"+(new Integer(i).toString()));
+			energyProducer.persist();
+		}
+		
+		assertTrue("Check if no of producers is correct", energyProducerRepository.count()==10);
+	
 		
 	}
 
