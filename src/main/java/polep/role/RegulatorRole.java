78579b49b81cea8121c3cf092892cfa30d52c760
PCBhagwat <P.C.Bhagwat@tudelft.nl>
2012-12-18 11:09:01 +0100

    Simplified regulatorrole so that checking is random everytime

--- src/main/java/polep/role/RegulatorRole.java
+++ src/main/java/polep/role/RegulatorRole.java
@@ -1,13 +1,12 @@
 package polep.role;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Set;
 
-import org.neo4j.graphdb.Direction;
+import java.util.ArrayList;
+
+
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.neo4j.annotation.RelatedTo;
-import org.springframework.data.repository.query.Param;
+
 import org.springframework.transaction.annotation.Transactional;
 
 
@@ -17,14 +16,12 @@ import agentspring.role.Role;
 import agentspring.role.RoleComponent;
 import polep.domain.agent.EnergyProducer;
 import polep.domain.agent.Regulator;
-import polep.domain.market.EnergyMarket;
-import polep.domain.technology.PowerPlant;
 import polep.repository.BidRepository;
 import polep.repository.EnergyProducerRepository;
 import polep.repository.PowerPlantRepository;
 
 /*<--- RegulatorRole: Prad ----->
-Regulator controls Powerplant owner:
+Regulator controls Power plant owner:
 	Throw a weighted coin (probability to control) to select owners for control (this is to simulate imperfect knowledge of the regulator)
 	Compare Bid volume with installed capacity
 	If Bid volume is less than installed capacity = Fine
@@ -43,47 +40,53 @@ public class RegulatorRole extends AbstractRole<Regulator> implements Role<Regul
 
 	@Autowired
 	BidRepository bidRepository;
-	
+
 
 	@Transactional
 	public void act(Regulator regulator){
-		
-		
+
+
 		ArrayList<EnergyProducer> listofpowerplantowners = energyProducerRepository.listofpowerplantowners(getCurrentTick());
 
 		double fine = regulator.getFine();
 		double capacity = 0;
-		
+
 
 		for (EnergyProducer currentProducer:listofpowerplantowners)
 		{
-		
-			BiasedCoin coin = new BiasedCoin(); // creates a coin
+			// creates a coin
 
-			coin.flip(currentProducer.getProbability()); 
+			BiasedCoin coin = new BiasedCoin(); 
+
+			// Simplified, so that the checking is random every time. Thus there is no bias
+
+			// coin.flip(currentProducer.getProbability()); 
+
+			coin.flip(0); 
 			double result = coin.getSide(); // flips coin gets value
 
 			if (result <= 0.5){
-				
-// Gets actual total capacity from the repository considering complete information (repository query may need correction)
-				
+
+				// Gets actual total capacity from the repository considering complete information (repository query may need correction)
+
 				capacity = plantRepository.calculateCapacityOfOperationalPowerPlants(getCurrentTick());
-				
-// Compares capacity with total of all bids of the producer for total tick. If it is < then fines the producer.			
-				
+
+				// Compares capacity with total of all bids of the producer for total tick. If it is < then fines the producer.			
+
 				if (capacity < energyProducerRepository.calculateTotalSupplyofProducerForTime(currentProducer, getCurrentTick())){
 					double cash = currentProducer.getCash() - fine;
 					currentProducer.setCash(cash);
 
-// Changes probability value so that next time the probabilty of checking is higher. Currently set to zero					
-					double probability = currentProducer.getProbability() + 0;
-					currentProducer.setProbability(probability);
+					// Changes probability value so that next time the probability of checking is higher. Currently set to zero					
+
+					// double probability = currentProducer.getProbability() + 0;
+					// currentProducer.setProbability(probability);
 
 				}
-// Is an else next needed here to tell the program to goto to the next generator
+				// Is an else next needed here to tell the program to goto to the next generator
 			}
 		}
 	}
-	}
+}
 
 
